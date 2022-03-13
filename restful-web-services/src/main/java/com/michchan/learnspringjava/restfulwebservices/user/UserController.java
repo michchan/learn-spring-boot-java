423e4f8c5d26be33f86b584b46b2951a0ea237ed
Michael Chan <michchandev@gmail.com>
2022-03-13 18:26:10 +0800

    feat: Add user resource

--- restful-web-services/src/main/java/com/michchan/learnspringjava/restfulwebservices/user/UserController.java
+++ restful-web-services/src/main/java/com/michchan/learnspringjava/restfulwebservices/user/UserController.java
@@ -1,26 +1,45 @@
 package com.michchan.learnspringjava.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 public class UserController {
 
     @Autowired
-    private UserDaoService service;
+    private UserDaoService userDaoService;
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
-        return service.findAll();
+        return userDaoService.findAll();
     }
 
     @GetMapping("/users/{id}")
     public User getUser(@PathVariable String id) {
-        return service.findOne(Integer.parseInt(id));
+        User user = userDaoService.findOne(Integer.parseInt(id));
+        if (user == null) {
+            throw new UserNotFoundException("id-" + id);
+        }
+        return user;
     }
 
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        User savedUser = userDaoService.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        // Return correct HTTP status code (201) and created location.
+        // e.g. `Location = http://localhost:8080/users/4` in response headers.
+        return ResponseEntity.created(location).build();
+    }
 }
