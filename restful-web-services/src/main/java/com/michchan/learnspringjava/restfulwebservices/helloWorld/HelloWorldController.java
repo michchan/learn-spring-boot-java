e18adc5cb797c70c909a7a1f2dc00c52675ebc16
Michael Chan <michchandev@gmail.com>
2022-03-29 20:09:11 +0800

    feat: add i18n to helloworld controller

--- restful-web-services/src/main/java/com/michchan/learnspringjava/restfulwebservices/helloWorld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/michchan/learnspringjava/restfulwebservices/helloWorld/HelloWorldController.java
@@ -1,9 +1,17 @@
 package com.michchan.learnspringjava.restfulwebservices.helloWorld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
+
     // @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -19,5 +27,14 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/{name}")
     public HelloWorldBean helloWorldBeanName (@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-i18n")
+    public String helloWorldInternationalized (
+        @RequestHeader(name = "Accept-Language", required = false) Locale locale
+    ) {
+        // @TODO: Handle CHAR encoding like with chinese characters
+        return messageSource
+                .getMessage("good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
     }
 }
