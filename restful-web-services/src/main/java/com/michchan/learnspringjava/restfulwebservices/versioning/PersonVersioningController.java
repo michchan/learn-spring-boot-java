f04a0325cb55689965db583a3a4bf4e9198715ba
Michael Chan <michchandev@gmail.com>
2022-04-22 17:53:08 +0800

    feat: add basic versioning (path-based)

---
+++ restful-web-services/src/main/java/com/michchan/learnspringjava/restfulwebservices/versioning/PersonVersioningController.java
@@ -0,0 +1,17 @@
+package com.michchan.learnspringjava.restfulwebservices.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersioningController {
+    @GetMapping("/v1/person")
+    public PersonV1 personV1() {
+        return new PersonV1("John Doe");
+    }
+
+    @GetMapping("/v2/person")
+    public PersonV2 personV2() {
+        return new PersonV2(new Name("John", "Doe"));
+    }
+}
