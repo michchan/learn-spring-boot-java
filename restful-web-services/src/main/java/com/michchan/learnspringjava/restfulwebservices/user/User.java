1808e6646ab37f1adcb7e2c2781fba9b25ddc20a
Michael Chan <michchandev@gmail.com>
2022-05-16 16:47:52 +0800

    docs: add swagger annotation to User

--- restful-web-services/src/main/java/com/michchan/learnspringjava/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/michchan/learnspringjava/restfulwebservices/user/User.java
@@ -1,15 +1,22 @@
 package com.michchan.learnspringjava.restfulwebservices.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@Schema(description = "All details about the user")
 public class User {
 
     private Integer id;
 
     @Size(min=2, max=100, message = "User's name must be characters with length between 2 to 100")
+    @Schema(description = "Name should have at least 2 characters")
     private String name;
 
+    @Past
+    @Schema(description = "Birthdate should be in the past")
     private Date birthdate;
 
     public User(Integer id, String name, Date birthdate) {
